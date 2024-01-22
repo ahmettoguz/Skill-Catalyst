9e453b1916d733d0c0731b305121d0e84aa7d36f
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-22 15:45:10 +0300

    User endpoint in progress.

---
+++ backend/src/endpoint/user/User.js
@@ -0,0 +1,33 @@
+const ExpressService = require("../../service/ExpressService");
+
+class User {
+  static async getUsers(req, res) {
+    return ExpressService.returnResponse(res, 200, "users");
+  }
+
+  static async getUserFromQueryString(req, res) {
+    // get data form query string
+    const userId = req.query.id;
+
+    return ExpressService.returnResponse(
+      res,
+      200,
+      "get specific user with id:",
+      userId
+    );
+  }
+
+  static async getUserFromParam(req, res) {
+    // get data form param from location
+    const userId = req.params.userId;
+
+    return ExpressService.returnResponse(
+      res,
+      200,
+      "get specific user with id:",
+      userId
+    );
+  }
+}
+
+module.exports = User;
