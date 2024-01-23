b36063d82e06b54640315337872cd5a589546506
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-01-23 15:10:37 +0300

    user-type crud operations in progress.

---
+++ backend/src/database/crud/user-type/update/Update.js
@@ -0,0 +1,29 @@
+const model = require("../../../model/model");
+
+class Update {
+  static async updateUser(userId, req) {
+    // TODO add check to new attributes
+    try {
+      const newAttributes = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phone: req.body.phone,
+        profileBanner: req.body.profileBanner,
+        profilePicture: req.body.profilePicture,
+      };
+      const filter = { _id: userId };
+
+      // TODO add check for newAttributes undefined null
+
+      // update user
+      await model.User.findOneAndUpdate(filter, newAttributes);
+
+      return true;
+    } catch (error) {
+      console.error("updateUser error: ", error);
+      return null;
+    }
+  }
+}
+
+module.exports = Update;
